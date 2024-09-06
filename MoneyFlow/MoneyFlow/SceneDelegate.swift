8d2da946baa9dc37ca9e87c57cc5adc4484c306c
Nahuel Borromeo <bborrome@emeal.nttdata.com>
2024-09-06 11:11:44 -0500

    -Disable dark mode

--- MoneyFlow/MoneyFlow/SceneDelegate.swift
+++ MoneyFlow/MoneyFlow/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         
         let tabBarController = TabBarController()
-        
+        window?.overrideUserInterfaceStyle = .light
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
