3a9a438c84215a04883c08985c64b82f25efbc90
Nahuel Borromeo <bborrome@emeal.nttdata.com>
2024-09-11 11:08:50 -0500

    -Create Expense and Income TableViewCell

---
+++ MoneyFlow/MoneyFlow/Scenes/Expenses/SubViews/ExpenseTableViewCell.swift
@@ -0,0 +1,75 @@
+//
+//  ExpenseTableViewCell.swift
+//  MoneyFlow
+//
+//  Created by Brian Nahuel Borromeo Da Costa on 9/09/24.
+//
+
+import UIKit
+
+final class ExpenseTableViewCell: UITableViewCell {
+    let detailLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let amountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .right
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+    private func setup() {
+        contentView.addSubview(detailLabel)
+        contentView.addSubview(amountLabel)
+        contentView.addSubview(dateLabel)
+        NSLayoutConstraint.activate([
+            detailLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            detailLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            detailLabel.trailingAnchor.constraint(equalTo: dateLabel.leadingAnchor, constant: -8),
+            detailLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            
+            dateLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            dateLabel.leadingAnchor.constraint(equalTo: detailLabel.trailingAnchor, constant: 8),
+            dateLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            
+            amountLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            amountLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            amountLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+        ])
+    }
+    
+    func setup(title: String, amount: String, date: Date) {
+        detailLabel.text = title
+        amountLabel.text = "S/. \(amount)"
+        dateLabel.text = DateUtils.formatDate(date)
+    }
+
+}
